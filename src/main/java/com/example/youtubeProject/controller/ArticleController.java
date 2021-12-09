c079e3b062fa0f1308f063a3cf88dd1a18322369
ysm7663-jn <ysm7663@gmail.com>
2021-12-09 16:37:23 +0900

    ArticleRepository DI 주입

--- src/main/java/com/example/youtubeProject/controller/ArticleController.java
+++ src/main/java/com/example/youtubeProject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.youtubeProject.controller;
 import com.example.youtubeProject.dto.ArticleForm;
 import com.example.youtubeProject.entity.Article;
 import com.example.youtubeProject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ArticleController {
 
+    @Autowired  // 스프링 부트가 미리 생성해놓은 객체를 가져다 자동 연결
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
@@ -23,9 +25,11 @@ public class ArticleController {
 
         // 1. Dto를 Entity로 변환
         Article article = form.toEntity();
+        System.out.println(article.toString());
 
         // 2. Repository에게 Entity를 DB에 저장하게 함
         Article save = articleRepository.save(article);
+        System.out.println(save.toString());
         return "";
     }
 }
