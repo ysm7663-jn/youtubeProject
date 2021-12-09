fa4dacd91849ca8a1f3458ad7f4772c18e983a4b
ysm7663-jn <ysm7663@gmail.com>
2021-12-09 17:23:13 +0900

    lombok을 활용한 리팩토링

--- src/main/java/com/example/youtubeProject/entity/Article.java
+++ src/main/java/com/example/youtubeProject/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.youtubeProject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id
@@ -17,13 +22,17 @@ public class Article {
 
     @Column
     private String content;
-
+    /*
+    @AllArgsConstructor로 대체
     public Article(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
     }
+    */
 
+    /*
+    @ToString으로 대체
     @Override
     public String toString() {
         return "Article{" +
@@ -32,4 +41,6 @@ public class Article {
                 ", content='" + content + '\'' +
                 '}';
     }
+    */
+
 }
