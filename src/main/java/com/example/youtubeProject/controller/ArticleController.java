fc26b516874942e3619548b4a23e96c53c5435c3
ysm7663-jn <ysm7663@gmail.com>
2021-12-09 16:34:21 +0900

    Create JPA 적용

--- src/main/java/com/example/youtubeProject/controller/ArticleController.java
+++ src/main/java/com/example/youtubeProject/controller/ArticleController.java
@@ -1,12 +1,16 @@
 package com.example.youtubeProject.controller;
 
 import com.example.youtubeProject.dto.ArticleForm;
+import com.example.youtubeProject.entity.Article;
+import com.example.youtubeProject.repository.ArticleRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -16,6 +20,12 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
+
+        // 1. Dto를 Entity로 변환
+        Article article = form.toEntity();
+
+        // 2. Repository에게 Entity를 DB에 저장하게 함
+        Article save = articleRepository.save(article);
         return "";
     }
 }
