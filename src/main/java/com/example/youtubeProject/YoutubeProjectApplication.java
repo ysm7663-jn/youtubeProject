4855e9a8908d35b87b0f033bd1c952e8d34fd8be
ysm7663-jn <ysm7663@gmail.com>
2021-12-09 14:46:09 +0900

    Initial commit

---
+++ src/main/java/com/example/youtubeProject/YoutubeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtubeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/youtubeProject/YoutubeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtubeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
