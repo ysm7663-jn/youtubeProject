77ed7c505ae4ee928e6b69f827dde43bf9347ca0
ysm7663-jn <ysm7663@gmail.com>
2021-12-11 15:08:42 +0900

    ArrayList<Article> 오버라이드

--- src/main/java/com/example/youtubeProject/repository/ArticleRepository.java
+++ src/main/java/com/example/youtubeProject/repository/ArticleRepository.java
@@ -3,6 +3,11 @@ package com.example.youtubeProject.repository;
 import com.example.youtubeProject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     // <관리대상 엔티티, 대표 값>
+
+    @Override
+    ArrayList<Article> findAll();
 }
