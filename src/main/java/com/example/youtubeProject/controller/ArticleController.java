68ee2667f32c39650df5887543058ff862a2e77d
ysm7663-jn <ysm7663@gmail.com>
2021-12-11 14:35:49 +0900

    id로 데이터 조회

--- src/main/java/com/example/youtubeProject/controller/ArticleController.java
+++ src/main/java/com/example/youtubeProject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.youtubeProject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j  // 로깅을 위한 @
@@ -40,5 +44,20 @@ public class ArticleController {
         log.info(save.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id로 데이터를 가져옴
+        // Optional <Article> articleEntity = articleRepository.findById(id);
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 보여줄 페이지를 설
+      return "articles/show";
     }
 }
