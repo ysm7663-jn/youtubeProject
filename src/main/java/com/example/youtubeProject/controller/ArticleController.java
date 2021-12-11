1cc0a14135089764ddea13d10fd6ec2a39172746
ysm7663-jn <ysm7663@gmail.com>
2021-12-11 17:21:04 +0900

    edit 기능 추가

--- src/main/java/com/example/youtubeProject/controller/ArticleController.java
+++ src/main/java/com/example/youtubeProject/controller/ArticleController.java
@@ -78,4 +78,17 @@ public class ArticleController {
         // 3. 뷰 페이지를 설정
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+
+        // 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터를 등록
+        model.addAttribute("article", articleEntity);
+
+        // 뷰 페이지 설정
+        return "articles/edit";
+    }
 }
