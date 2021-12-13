6db988ed8cc4c43fc8c5f14e30f50b90615512b6
ysm7663-jn <ysm7663@gmail.com>
2021-12-13 10:57:31 +0900

    toEntity() id 수정

--- src/main/java/com/example/youtubeProject/dto/ArticleForm.java
+++ src/main/java/com/example/youtubeProject/dto/ArticleForm.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 @ToString
 public class ArticleForm {
 
+    private Long id;
     private String title;
     private String content;
 
@@ -32,6 +33,6 @@ public class ArticleForm {
      */
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
