cf4c8b459463fcd8d64c5db0eb6a2b851898797e
ysm7663-jn <ysm7663@gmail.com>
2021-12-14 10:27:45 +0900

    회원가입 구현 To do : 비밀번호 체크, 이메일 인증, ID 중복확인

--- src/main/java/com/example/youtubeProject/controller/UserController.java
+++ src/main/java/com/example/youtubeProject/controller/UserController.java
@@ -1,16 +1,46 @@
 package com.example.youtubeProject.controller;
 
+import com.example.youtubeProject.dto.UserDto;
+import com.example.youtubeProject.entity.User;
+import com.example.youtubeProject.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/users/login")
-    public String index() {
-        return "/users/login";
+    public String loginPage() {
+        return "users/login";
     }
 
+    @GetMapping("/users/new")
+    public String createPage() {
+        return "users/new";
+    }
 
+    @PostMapping("/users/createId")
+    public String createId(UserDto dto) {
+
+        log.info(dto.toString());
+
+        // 1. Dto >> Entity
+        User user = dto.toEntity();
+
+        log.info(user.toString());
+
+        // 2. Entity >> Repository
+        User save = userRepository.save(user);
+        log.info(save.toString());
+
+        return "redirect:/users/login";
+    }
 
 }
